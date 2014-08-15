d05d801c09cc0542dead1e936d78979428410771
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-15 00:47:04 -0400

    Moved objects to their rightful place.

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,20 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+    	return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+    	var r = [];
+      	for(p in obj) {
+        	if(obj.hasOwnProperty(p))
+          	r[r.length] = p + ': ' + obj[p];
+      	}
+      return r;
     }
   };
 });
