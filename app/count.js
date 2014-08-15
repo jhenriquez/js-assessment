82e53e49dba908513bc0c4743bf30622d57b3aee
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-15 01:30:26 -0400

    Very poor implementation of the counter.

--- app/count.js
+++ app/count.js
@@ -3,6 +3,19 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+    	return new function () {
+    		this.cancel = function () {
+    			clearInterval(intervalId);
+    		};
+
+    		console.log(start);
+    		var intervalId = setInterval(function () {
+    			start++;
+    			console.log(start);
+    			if(start >= end)
+    				clearInterval(intervalId);
+    		},100);
+    	};
     }
   };
 });
