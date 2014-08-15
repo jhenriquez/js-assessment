397c70ce48582261c8ea22a7aa470ce9edd877f6
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-15 00:18:09 -0400

    Completed and moved answers to their rightful place.

--- app/arrays.js
+++ app/arrays.js
@@ -2,60 +2,111 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    indexOf : function(arr, item) {
-
+    indexOf : function(a, item) {
+        for(var i = 0; i < a.length; i++)
+            if(a[i] === item)
+                return i;
+        return -1;
     },
 
-    sum : function(arr) {
-
+    sum : function(a) {
+        var acc = 0;
+        for(var i = 0; i < a.length; i++)
+            acc += a[i];
+        return acc;
     },
 
-    remove : function(arr, item) {
-
+    remove : function(a, item) {
+        var cp = [];
+        for(var i = 0; i < a.length; i++) {
+            if(a[i] !== item)
+                cp.push(a[i]);
+        }
+        return cp;
     },
 
-    removeWithoutCopy : function(arr, item) {
-
+    removeWithoutCopy : function(a, item) {
+        for(var i = a.length-1; i >= 0; i--) {
+            if (a[i] === item)
+                a.splice(i,1);
+        }
+        return a;
     },
 
     append : function(arr, item) {
-
+        arr[arr.length] = item;
+        return arr;
     },
 
-    truncate : function(arr) {
-
+    truncate : function(a) {
+        var cp = [];
+        for(var i = 0; i < a.length-1; i++)
+            cp.push(a[i]);
+        return cp;
     },
 
-    prepend : function(arr, item) {
-
+    prepend : function(a, item) {
+        var cp = [item];
+        for(var i = 0; i < a.length; i++)
+            cp.push(a);
+        return cp;
     },
 
-    curtail : function(arr) {
-
+    curtail : function(a) {
+        var cp = [];
+        for(var i = 1; i < a.length; i++)
+            cp.push(a[i]);
+        return cp;
     },
 
-    concat : function(arr1, arr2) {
+    concat : function(a, b) {
+        for(var i = 0; i < b.length; i++)
+            a.push(b[i]);
+        return a;
+    },  
 
+    insert : function(a, item, index) {
+        var cp = [];
+        for(var i = 0; i < a.length; i++) {
+            if(cp.length === index)
+        cp.push(item);
+        cp.push(a[i]);
+      }
+      return cp;
     },
 
-    insert : function(arr, item, index) {
-
-    },
-
-    count : function(arr, item) {
-
+    count : function(a, item) {
+        var count = 0;
+        for(var i = 0; i < a.length; i++) {
+            if(a[i] === item) ++count;
+        }
+        return count;
     },
 
     duplicates : function(arr) {
-
+        var dups = [];
+        for (var i = 0; i < arr.length; i++) {
+            if(this.count(arr,arr[i]) > 1 && this.count(dups,arr[i]) === 0)
+                dups[dups.length] = arr[i];
+        }
+        return dups;
     },
 
-    square : function(arr) {
-
+    square : function(a) {
+        var sqrt = [];
+        for(var i = 0; i < a.length; i++) {
+            sqrt.push(a[i]*a[i]);
+        }
+        return sqrt;
     },
 
     findAllOccurrences : function(arr, target) {
-
+        var occ = [];
+        for(var i = 0; i < a.length; i++) {
+            if(a[i] === target)
+                occ.push(i);
+        }
+        return occ;
     }
   };
 });
