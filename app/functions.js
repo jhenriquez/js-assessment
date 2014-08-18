1728ca240e9370d75606d07d0513f2d34f63de45
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-18 06:26:27 -0400

    Almost completed! Wow, I don't know currying.

--- app/functions.js
+++ app/functions.js
@@ -17,27 +17,47 @@ define(function() {
     },
 
     makeClosures : function(arr, fn) {
-
+      var rs = [];
+      arr.forEach(function (value) {
+        rs[rs.length] = function () { return fn(value); };
+      });
+      return rs;
     },
 
     partial : function(fn, str1, str2) {
-
+        return function (str) { return fn (str1, str2, str) };
     },
 
     useArguments : function() {
-
+        var acc = 0;
+        for(var i = 0; i < arguments.length; i++)
+            acc += arguments[i];
+        return acc;
     },
 
     callIt : function(fn) {
-
+        var args = [];
+        for(var i = 1; i < arguments.length; i++)
+            args[args.length] = arguments[i];
+        return fn.apply(this, args);
     },
 
     partialUsingArguments : function(fn) {
-
+        var preSet = [];
+        for(var i = 1; i < arguments.length; i++)
+            preSet[preSet.length] = arguments[i];
+        return function () {
+            var args = [];
+            for(var i = 0; i < arguments.length; i++)
+                args[args.length] = arguments[i];
+            return fn.apply(this, preSet.concat(args));
+        };
     },
 
     curryIt : function(fn) {
-
+        return function (arg) {
+            return fn(n)
+        };
     }
   };
 });
