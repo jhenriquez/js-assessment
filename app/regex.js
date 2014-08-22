e0126652908796809fa9b12a5a67b5b82e620bb9
Julio Henriquez <julio.m.henriquez@gmail.com>
2014-08-22 18:02:39 -0400

    Added some regex.

--- app/regex.js
+++ app/regex.js
@@ -3,15 +3,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return (/\d/).test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        return (/([A-Za-z])\1/).test(str);
     },
 
     endsWithVowel : function(str) {
-
     },
 
     captureThreeNumbers : function(str) {
