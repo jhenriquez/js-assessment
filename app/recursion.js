18627240bd1df7c1aa0c6e60e22c685e3d7add04
jhenriquez <julio.m.henriquez@gmail.com>
2014-08-25 23:35:55 -0400

    Recursion completed.

--- app/recursion.js
+++ app/recursion.js
@@ -3,7 +3,33 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     listFiles: function(data, dirName) {
+    	var filesOnly = [];
+    	
+    	function processDir (dir) {
+    		if (!dir) return;
+    		dir.files.forEach(function(value) {
+	    		if (value.files)
+					return processDir(value, true);
 
+	    		if (filesOnly.indexOf(value) <= -1)
+	    			filesOnly[filesOnly.length] = value;
+    		});
+    	};
+    	
+    	function locateDir (dir) {
+    		if(dir.dir === dirName)
+    			return dir;
+    		for (var i = 0; i < dir.files.length; i++)
+    			if (dir.files[i].files) return locateDir (dir.files[i]);
+    	};
+
+    	if (dirName) {
+    		processDir (locateDir (data));
+    	} else  {
+    		processDir (data);
+    	}
+
+    	return filesOnly;
     },
 
     permute: function(arr) {
